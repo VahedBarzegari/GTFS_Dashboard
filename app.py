6452e977fabc94d3573fc9b766a1394ead980ec3
Vahed Barzegari <vahed811@gmail.com>
2025-01-29 22:02:53 -0500

    app modification

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ ui.tags.style(
             display: flex;
             align-items: center;
             justify-content: center;
+            background-color: black;
             height: 90px;
         }
 
@@ -98,7 +99,7 @@ with ui.div(class_="header-container"):
 ############################################
 with ui.card():
     
-    ui.card_header("General Information", style="text-align: center; font-size: 24px;")
+    ui.card_header("General Information")
     with ui.layout_columns(col_widths={"sm": (3,3,3,3)}):
 
         with ui.value_box(
@@ -109,7 +110,7 @@ with ui.card():
 
             @render.ui  
             def datestartfun():  
-                return "November 17, 2024"  
+                return "January 05, 2025"  
             
 
         with ui.value_box(
@@ -120,7 +121,7 @@ with ui.card():
 
             @render.ui  
             def dateendfun():  
-                return "December 22, 2024"  
+                return "February 16, 2025"  
             
 
 
@@ -132,7 +133,7 @@ with ui.card():
 
             @render.ui  
             def routefun():  
-                return "215"  
+                return "217"  
             
 
         with ui.value_box(
@@ -143,7 +144,7 @@ with ui.card():
 
             @render.ui  
             def stopfun():  
-                return "9308"  
+                return "9369"  
 
 ############################################
 
@@ -246,7 +247,7 @@ with ui.layout_columns(
             legend_html = '''
             <div style="
                 position: fixed; 
-                bottom: 20px; left: 20px; width: 200px; height: auto; 
+                bottom: 20px; left: 20px; width: 100px; height: auto; 
                 background-color: white; z-index:9999; font-size:14px;
                 border-radius: 5px; padding: 10px; box-shadow: 2px 2px 5px rgba(0,0,0,0.3);
             ">
@@ -258,7 +259,34 @@ with ui.layout_columns(
             return m
 
 
-with ui.layout_columns(col_widths={"sm": (8,4)}, height='400px'):
+with ui.layout_columns(col_widths={"sm": (4,8)}, height='400px'):
+
+
+    with ui.card():
+        ui.card_header("Explanation")
+
+
+        @render.ui
+        def dynamic_content():
+            #if input.tab() == "agency":
+            return ui.TagList(
+                ui.h6("List of Feeds"),
+                ui.tags.ul(  # Unordered list for bullet points
+                    ui.tags.li("agency"),
+                    ui.tags.li("calendar"),
+                    ui.tags.li("calendar_dates"),
+                    ui.tags.li("routes"),
+                    ui.tags.li("trips"),
+                    ui.tags.li("stop_times"),
+                    ui.tags.li("shapes"),
+                    ui.tags.li("stops"),
+                    ui.tags.li("feed_info")
+
+                ),
+                ui.p("Explore feed tables or refer to ", 
+                    ui.a("ttc.ca", href="https://www.ttc.ca", target="_blank"))
+            )
+       
 
     with ui.navset_card_underline(id="tab", placement='above', title="GTFS Feeds"):
 
@@ -286,7 +314,7 @@ with ui.layout_columns(col_widths={"sm": (8,4)}, height='400px'):
                 def sample_sales_data4():
                     return render.DataGrid(routes_df, selection_mode="row", filters=True)
 
-        with ui.nav_menu("Other links"):
+        with ui.nav_menu("Other feeds"):
             with ui.nav_panel("Stops"):
                 with ui.card():
                     @render.data_frame
@@ -305,27 +333,7 @@ with ui.layout_columns(col_widths={"sm": (8,4)}, height='400px'):
                     def sample_sales_data7():
                         return render.DataGrid(trips_df.head(100), selection_mode="row", filters=True)
 
-    with ui.card():
-        ui.card_header("Explanation")
-
-
-        @render.ui
-        def dynamic_content():
-            #if input.tab() == "agency":
-            return ui.TagList(
-                ui.p("The GTFS feeds can provide valuable information."),
-                ui.h6("List of Feeds"),
-                ui.tags.ul(  # Unordered list for bullet points
-                    ui.tags.li("agency"),
-                    ui.tags.li("calendar"),
-                    ui.tags.li("routes"),
-                    ui.tags.li("trips"),
-                    ui.tags.li("stop_times"),
-                ),
-                ui.p("Explore feed tables or refer to ", 
-                    ui.a("ttc.ca", href="https://www.ttc.ca", target="_blank"))
-            )
-            #else:
+      #else:
                 #return ui.h3("kiiiiiir")
 
 with ui.card():
@@ -335,7 +343,7 @@ with ui.card():
 
             with ui.card():
                 ui.card_header("Select Date")
-                ui.input_date("date", "", min='2024-11-17', max='2024-12-22')  
+                ui.input_date("date", "", min='2025-01-05', max='2025-02-16')  
 
                 
 
@@ -417,66 +425,76 @@ with ui.card():
             # Dynamically render the result
             with ui.card():
                 
-                ui.card_header("Routes")
+                ui.card_header("Top Routes")
                 Date_Classification_df['date'] = pd.to_datetime(Date_Classification_df['date']).dt.date  
 
                 @render.data_frame
                 def sample_sales_data71():
-                    selected_date = input.date()  # Ensure date selection updates the table
+                    if input.date():
+                        selected_date = input.date()  # Ensure date selection updates the table
 
-                    # Fetch active services
-                    active_services = Date_Classification_df.loc[Date_Classification_df['date'] == selected_date, 'active_services'].values
-                    filtered_stop_time = pd.DataFrame()  # Initialize as empty DataFrame
+                        # Fetch active services
+                        active_services = Date_Classification_df.loc[Date_Classification_df['date'] == selected_date, 'active_services'].values
+                        filtered_stop_time = pd.DataFrame()  # Initialize as empty DataFrame
 
-                    if len(active_services) > 0:
-                        active_services = active_services[0]  
+                        if len(active_services) > 0:
+                            active_services = active_services[0]  
 
-                        if isinstance(active_services, str):  
-                            active_services = ast.literal_eval(active_services)  # Convert string to list
+                            if isinstance(active_services, str):  
+                                active_services = ast.literal_eval(active_services)  # Convert string to list
+                            
+                            if not isinstance(active_services, list):  
+                                active_services = [active_services]  # Ensure it's a list
+
+                            filtered_stop_time = modified_stop_times_df[modified_stop_times_df['service_id'].isin(active_services)]
+
+                        route_dataframe = filtered_stop_time.groupby('route_id').agg({
+                            'trip_id': 'nunique',
+                            'block_id': 'nunique',
+
+                        }).reset_index()
+
+
+
+                        route_dataframe = route_dataframe.merge(route_df1[['route_id','route_short_name','route_long_name','route_type']],on='route_id',how='left')
+                        route_type_mapping = {
+                            0: "Streetcar",
+                            1: "Subway",
+                            2: "Rail",
+                            3: "Bus",
+                            4: "Ferry",
+                            5: "Cable Tram",
+                            6: "Aerial Lift",
+                            7: "Funicular",
+                            11: "Trolleybus",
+                            12: "Monorail"
+                        }
+                        route_dataframe['mode'] = route_dataframe['route_type'].map(route_type_mapping)
+
+                        route_dataframe = route_dataframe.drop('route_type', axis= 1)
+
+                        if input.sort()== "1":
+
+                            sorted_routes = route_dataframe.sort_values(by="trip_id", ascending=False).reset_index(drop=True)
+                        else:
+                            sorted_routes = route_dataframe.sort_values(by="block_id", ascending=False).reset_index(drop=True)
                         
-                        if not isinstance(active_services, list):  
-                            active_services = [active_services]  # Ensure it's a list
 
-                        filtered_stop_time = modified_stop_times_df[modified_stop_times_df['service_id'].isin(active_services)]
+                        sorted_routes.rename(columns={"trip_id": "no. of trips", "block_id": "no. of blocks"}, inplace=True)
+                        sorted_routes.rename(columns={"route_short_name": "short name", "route_long_name": "route name"}, inplace=True)
+                        sorted_routes = sorted_routes.drop('short name',axis=1)
+                        sorted_routes = sorted_routes.drop('route_id',axis=1)
+                        sorted_routes = sorted_routes[['route name','no. of trips', 'no. of blocks','mode']]
+                        top_routes = sorted_routes.iloc[:input.n(), :]
 
-                    route_dataframe = filtered_stop_time.groupby('route_id').agg({
-                        'trip_id': 'nunique',
-                        'block_id': 'nunique',
+                        return render.DataGrid(top_routes.head(100), selection_mode="row", filters=False)
 
-                    }).reset_index()
-
-
-                    # Find the route with the maximum number of trips
-                    max_trips_route_id = route_dataframe.loc[route_dataframe['trip_id'].idxmax(), 'route_id']
-                    max_trips_route_value = route_dataframe.loc[route_dataframe['trip_id'].idxmax(), 'trip_id']
-
-                    route_dataframe = route_dataframe.merge(route_df1[['route_id','route_short_name','route_long_name','route_type']],on='route_id',how='left')
-                    route_type_mapping = {
-                        0: "Streetcar",
-                        1: "Subway",
-                        2: "Rail",
-                        3: "Bus",
-                        4: "Ferry",
-                        5: "Cable Tram",
-                        6: "Aerial Lift",
-                        7: "Funicular",
-                        11: "Trolleybus",
-                        12: "Monorail"
-                    }
-                    route_dataframe['mode'] = route_dataframe['route_type'].map(route_type_mapping)
-
-                    route_dataframe = route_dataframe.drop('route_type', axis= 1)
-
-                    sorted_routes = route_dataframe.sort_values(by="trip_id", ascending=False).reset_index(drop=True)
-                    sorted_routes.rename(columns={"trip_id": "no. of trips", "block_id": "no. of blocks"}, inplace=True)
-                    sorted_routes.rename(columns={"route_short_name": "short name", "route_long_name": "route name"}, inplace=True)
-                    sorted_routes = sorted_routes.drop('short name',axis=1)
-                    sorted_routes = sorted_routes.drop('route_id',axis=1)
-                    sorted_routes = sorted_routes[['route name','no. of trips', 'no. of blocks','mode']]
-                    top_routes = sorted_routes.iloc[:input.n(), :]
-
-                    return render.DataGrid(top_routes.head(100), selection_mode="row", filters=False)
             ui.input_slider("n", "Number of Routes", 1, 10, 5)
+            ui.input_radio_buttons(  
+                "sort",  
+                "",  
+                {"1": "Sort by Trips", "2": "Sort by Blocks"},  
+            )
          
         with ui.card():
 
@@ -585,16 +603,16 @@ with ui.card():
                         plt.grid(axis='y', linestyle='--', alpha=0.7)
 
             with ui.card():
-                ui.card_header("Routes")
+                ui.card_header("Trip Frequency of Routes")
+                
+                
+                
+                Date_Classification_df['date'] = pd.to_datetime(Date_Classification_df['date']).dt.date  
+
+               
                 @render.plot
-                def plot_day_period12():
-
+                def sample_sales_data711():
                     if input.date():
-
-
-                        Date_Classification_df['date'] = pd.to_datetime(Date_Classification_df['date']).dt.date
-
-
                         selected_date = input.date()  # Ensure date selection updates the table
 
                         # Fetch active services
@@ -612,80 +630,50 @@ with ui.card():
 
                             filtered_stop_time = modified_stop_times_df[modified_stop_times_df['service_id'].isin(active_services)]
 
+                        route_dataframe = filtered_stop_time.groupby('route_id').agg({
+                            'trip_id': 'nunique',
+                            'block_id': 'nunique',
 
-                        # Assuming 'time_column' is the column containing the time in the format '12:25:04'
-                        filtered_stop_time.loc[:, 'arivalTime_seconds'] = filtered_stop_time['arrival_time'].apply(
-                            lambda x: sum(int(i) * 60**(2 - idx) for idx, i in enumerate(x.split(':')))
-                        )
-                        filtered_stop_time.loc[:, 'departureTime_seconds'] = filtered_stop_time['departure_time'].apply(
-                            lambda x: sum(int(i) * 60**(2 - idx) for idx, i in enumerate(x.split(':')))
-                        )
-                        column_order=['route_id','service_id','trip_id','block_id','shape_id','arrival_time','arivalTime_seconds','departureTime_seconds','stop_id','stop_lat','stop_lon','stop_sequence']
-
-                        filtered_trips = filtered_stop_time[column_order]
+                        }).reset_index()
 
 
-                        # Start and end time intervals
-                        start_time_intervals = [filtered_trips['arivalTime_seconds'].min(), 6 * 3600, 9 * 3600, 15 * 3600, 17 * 3600, 19 * 3600, 22 * 3600]
-                        end_time_intervals = start_time_intervals[1:] + [filtered_trips['arivalTime_seconds'].max()]  
 
-                        # Initialize dictionaries to store results
-                        trips_in_day_period = {}
-                        selected_trips_dict = {}
+                        route_dataframe = route_dataframe.merge(route_df1[['route_id','route_short_name','route_long_name','route_type']],on='route_id',how='left')
+                        route_type_mapping = {
+                            0: "Streetcar",
+                            1: "Subway",
+                            2: "Rail",
+                            3: "Bus",
+                            4: "Ferry",
+                            5: "Cable Tram",
+                            6: "Aerial Lift",
+                            7: "Funicular",
+                            11: "Trolleybus",
+                            12: "Monorail"
+                        }
+                        route_dataframe['mode'] = route_dataframe['route_type'].map(route_type_mapping)
 
-                        # Iterate through time intervals
-                        for start, end in zip(start_time_intervals, end_time_intervals):
-                            # Select trips that start within the current interval
-                            selected_trips = filtered_trips[
-                                (filtered_trips['arivalTime_seconds'] >= start) & 
-                                (filtered_trips['arivalTime_seconds'] < end) & 
-                                (filtered_trips['stop_sequence'] == 1)
-                            ]
-                            
-                            # Count the number of unique trips within the interval
-                            num_trips = selected_trips['trip_id'].nunique()
-                            
-                            # Calculate the duration of the interval
-                            duration = (end - start)/3600
+                        route_dataframe = route_dataframe.drop('route_type', axis= 1)
 
-                            last_time_interval = int(start_time_intervals[-1]/3600)
-
-                            if start == last_time_interval*3600:
-                                duration = round(selected_trips['arivalTime_seconds'].max()/3600)-start/3600
-
-                            
-                            # Store the selected trips in the dictionary
-                            day_period_key = f'{start//3600:02d}:00 - {end//3600:02d}:00' if start < last_time_interval * 3600 else f'{start//3600:02d}:00 - End of Service'
-                            trips_in_day_period[day_period_key] = {'num_trips': num_trips, 'duration': duration,'num_trip_per_hour': round(num_trips/duration)}
-                            
-
-
-                        # Extract intervals and trips per hour for plotting
-                        intervals = list(trips_in_day_period.keys())
-                        num_trips_per_hour = [data['num_trip_per_hour'] for data in trips_in_day_period.values()]
-
-                        # Find the index of the maximum value (number of trips per hour)
-                        max_index = num_trips_per_hour.index(max(num_trips_per_hour))
-
-                        # Create the bar plot
-                        #plt.figure(figsize=(8, 6))  # Set the figure size
-                        bars = plt.bar(intervals, num_trips_per_hour, color='skyblue', edgecolor='black', width=0.5)
-
-                        # Highlight the maximum bar in red
-                        bars[max_index].set_color('red')
-                        bars[max_index].set_edgecolor('black')
-
-
-                        # Customize the font and appearance
-                        plt.xlabel("Day Period", fontsize=12, fontfamily='Times New Roman')
-                        plt.ylabel("Number of Trips per Hour", fontsize=12, fontfamily='Times New Roman')
-                        plt.xticks(rotation=30, fontsize=10, fontfamily='Times New Roman')  # Rotate the x-axis labels for better readability
-                        plt.yticks(fontsize=10, fontfamily='Times New Roman')
-
-                        # Add grid lines for better readability
-                        plt.grid(axis='y', linestyle='--', alpha=0.7)
 
                     
+
+                   
+
+                        # Create the plot
+                        
+                        hist_values, bins, patches = plt.hist(route_dataframe['trip_id'], bins=10, color='orange', edgecolor='black')
+
+                        # Add labels above each bar
+                        for patch, value in zip(patches, hist_values):
+                            x = patch.get_x() + patch.get_width() / 2
+                            y = value
+                            plt.text(x, y + 0.5, f'{int(value)}', ha='center', va='bottom', fontsize=8)
+
+                        # Customize labels and title
+                        plt.xlabel('Number of Trips', fontsize=12)
+                        plt.ylabel('Number of Routes', fontsize=12)
+                        
 
 
 
